33bc70e0fa0c059cb6db564fb166ea1f4802b7b2
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-05 23:19:08 +1000

    custom action1

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import core from '@actions/core';
+import github from '@actions/github';
+import exec from '@actions/exec';
 
 function run() {
     core.notice('hello from the action...');
