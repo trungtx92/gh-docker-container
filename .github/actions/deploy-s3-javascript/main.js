7e4a99441e569632cc42d7973f55e7d85a605f53
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-05 23:47:13 +1000

    install js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-import core from '@actions/core';
-import github from '@actions/github';
-import exec from '@actions/exec';
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
     core.notice('hello from the action...');
