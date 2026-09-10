0f3fa32a8a1bdb7f3ecf0997bb674c7b46782c7f
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-10 11:08:50 +1000

    DSS-0001: push code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('hello from the action...');
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload the files to the S3 bucket
+    const s3Uri = `s3://${bucket}`;
+    AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+    AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('Hello from deploy-s3-javascript action!');
 }
 
 run();
