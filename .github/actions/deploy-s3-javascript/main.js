8c98cb8ed1be8beb03edc4955e6d0fe7181ae1d5
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-11 11:27:52 +1000

    DSS-0001: update the key

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,7 +10,7 @@ function run() {
     // 2) Upload the files to the GCS bucket
     const gcs = `gs://${bucket}`;
     // GCP_ACCESS_KEY_ID = process.env.GCP_ACCESS_KEY_ID;
-    GCP_SECRET_ACCESS_KEY = process.env.GCP_SECRET_ACCESS_KEY;
+    GCP_SERVICE_ACCOUNT_KEY = process.env.GCP_SERVICE_ACCOUNT_KEY;
     exec.exec(`gsutil -m rsync -r ${distFolder} ${gcs}`);
 
     core.notice('Hello from deploy-s3-javascript action!');
