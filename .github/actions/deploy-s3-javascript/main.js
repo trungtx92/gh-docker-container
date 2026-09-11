880ba46b1cc4490aaf51327c0ad44a40da444559
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2026-09-11 10:49:38 +1000

    DSS-0001: update main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,15 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    const bucket = core.getInput('bucket', { required: true });
+    const bucket = core.getInput('bucket-name', { required: true });
     const bucketRegion = core.getInput('bucket-region', { required: true });
     const distFolder = core.getInput('dist-folder', { required: true });
 
-    // 2) Upload the files to the S3 bucket
-    const s3Uri = `s3://${bucket}`;
-    AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-    AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    // 2) Upload the files to the GCS bucket
+    const gcs = `gs://${bucket}`;
+    GCP_ACCESS_KEY_ID = process.env.GCP_ACCESS_KEY_ID;
+    GCP_SECRET_ACCESS_KEY = process.env.GCP_SECRET_ACCESS_KEY;
+    exec.exec(`gsutil -m rsync -r ${distFolder} ${gcs}`);
 
     core.notice('Hello from deploy-s3-javascript action!');
 }
